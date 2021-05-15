692570bbfb189496333c3c75a497e0197a82a261
ho1m <mohamed.yousef@utb.ecutbildning.se>
2021-05-15 19:23:58 +0200

    first commit

---
+++ digitaltcvapp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    AboutMeComponent,
+    ContactMeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomepageComponent},
+      {path: 'aboutme', component: AboutMeComponent},
+      {path: 'contactme', component: ContactMeComponent},
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
